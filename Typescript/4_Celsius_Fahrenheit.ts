b01f2aedc29f90aaf6d7c7e2574a75d72b6484b3
GustavoTeixeira0 <gustavoteixeiram@unipam.edu.br>
2025-06-19 11:39:23 -0300

    Celsius Fahrenheit

---
+++ Typescript/4_Celsius_Fahrenheit.ts
@@ -0,0 +1,22 @@
+import * as readline from "readline";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+function calcularTemperatura(celsius: number): number {
+    return (celsius * 9) / 5 + 32;
+}
+
+rl.question("Digite a temperatura em graus Celsius: ", (input) => {
+    const celsius = parseFloat(input);
+
+    if(isNaN(celsius)){
+        console.log("Por favor, digite um número válido.");
+    } else {
+        const fahrenheit = calcularTemperatura(celsius);
+        console.log(`${celsius} graus Celsius é igual a ${fahrenheit.toFixed(2)} graus Fahrenheit.`);
+    }
+    rl.close();
+});
